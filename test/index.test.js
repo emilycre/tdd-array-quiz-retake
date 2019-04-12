8ee2fea6313d0b70000a0b7608b5bacbf93e9318
Emily Creamer <emily.creamer@icloud.com>
2019-04-12 15:19:35 -0700

    second test, eslint error

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,19 @@
-const wowIsbig = require('../lib/index');
+const wowIsBig = require('../lib/index');
 
 describe('wowIsBig', () => {
   it('removes numbers less than 5', () => {
-    const expected = [];
-    const input = [];
+    const expected = [1, 3, 2];
+    const input = [1, 3, 5, 7, 2];
     const result = wowIsBig(input);
 
     expect(result).toEqual(expected)
+  });
+
+  it('output is "wow, 7 is big!"', () => {
+    const expected = ['Wow, 7 is big!'];
+    const input = [1, 3, 5, 7, 2];
+    const result = wowIsBig(input);
+
+    expect(result).toEqual(expected);
   });
 });
